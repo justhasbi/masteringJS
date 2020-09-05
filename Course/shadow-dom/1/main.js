d5f45569652570cc0989dcf84100d8afd28ec0d4
justhasbi <hasbishuhada123@gmail.com>
2020-09-05 17:21:25 +0700

    commit

---
+++ Course/shadow-dom/1/main.js
@@ -0,0 +1,15 @@
+// shadow HOST
+const divElement = document.createElement('div');
+
+// Elemen yang berada dalam shadow dom
+const headingElement = document.createElement('h1');
+headingElement.innerText = "Text di dalam shadow dom";
+
+// melampirkan Shadow Root pada shadow host menggunakan method attachShadow pada host
+// Mengatur mode shadow dengan nilai open
+const shadowRoot = divElement.attachShadow({mode: "open"});
+
+// memasukan element heading pada shadow root
+shadowRoot.appendChild(headingElement);
+
+document.body.appendChild(divElement);
